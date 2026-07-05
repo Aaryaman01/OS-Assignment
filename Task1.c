498a3c5094a5c190df9f08b4f490a561eba43656
Aaryaman01 <Aaryamanshakya11@gmail.com>
2026-07-05 10:41:22 +0000

    Implemented mutex synchronization, handled race conditions and demonstrated deadlock prevention

--- Task1.c
+++ Task1.c
@@ -5,6 +5,11 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+/* Mutex Synchronization mechanism applied */
+// Shared resource for Synchronization
+int counter = 0;
+pthread_mutex_t lock;
+
 /* Process creation (Parent and Child Process) */
 void processCreation(){
 	printf("\n[Process Creation] fork()\n");
@@ -34,28 +39,59 @@ void processCreation(){
 	}
 }
 /* Multiple Thread Creation */
-void *systemInfo(void *arg){
+void *systemInfo(void *arg){  // Display System Information
 	struct utsname sysInfo;
 	uname(&sysInfo);
+
+	/* Synchronization using Mutex */
+	pthread_mutex_lock(&lock);
+	counter++;
+
 	printf("[System Info Thread] OS: %s, Version: %s\n", sysInfo.sysname, sysInfo.version);
+
+	pthread_mutex_unlock(&lock);
+
 	pthread_exit(NULL);
 }
-void *diskIO(void *arg) {
+
+void *diskIO(void *arg) {  // Simulate Disk I/O
+
+	/* Lock shared resource */
+	pthread_mutex_lock(&lock);
 	printf("[Disk I/O Thread] Starting disk read operation...\n");
 	sleep(2);
+	counter++;
 	printf("[Disk I/O Thread] Disk read completed!\n");
+	printf("Counter : %d\n", counter);
+
+	pthread_mutex_unlock(&lock);
+
 	pthread_exit(NULL);
 }
-void *networkActivity(void *arg) {
+
+void *networkActivity(void *arg) {  // Simulate Network Activity
+
+	/* Lock shared resource */
+	pthread_mutex_lock(&lock);
+
 	printf("[Network Thread] Sending data over the network...\n");
 	sleep(1);
+	counter++;
 	printf("[Network Thread] Data sent successfully!\n");
 	pthread_exit(NULL);
 }
 
+
+
 int main(){
+	/* Process Creation*/
 	processCreation();
+
 	pthread_t thread1, thread2, thread3;
+
+	/* Initialize Mutex */
+	pthread_mutex_init(&lock, NULL);
+
 	printf("Main Process ID: %d\n", getpid());
 	pthread_create(&thread1, NULL, systemInfo, NULL);
 	pthread_create(&thread2, NULL, diskIO, NULL);
@@ -64,6 +100,20 @@ int main(){
 	pthread_join(thread2, NULL);
 	pthread_join(thread3, NULL);
 
+	printf("\n[Synchronization]\n");
+	printf("Final Counter Value: %d\n", counter);
+
+	printf("\n[Race Condition]\n");
+	printf("Race Condition is prevented using a mutex.\n");
+
+	printf("\n[Deadlock Prevention]\n");
+	printf("Deadlock is prevented by locking and unlocking the mutex properly.\n");
+
+	/* Destroy Mutex */
+	pthread_mutex_destroy(&lock);
+
 	printf("All OS tasks (threads) completed. Main process exiting.\n");
 	return 0;
 }
+
+
